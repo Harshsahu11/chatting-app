6ad0b72b40418c28a15398e166a696f078daba45
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-17 14:57:56 +0530

    Database setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const ConnectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB connected :${conn.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB Connection error : ",error);
+    }
+};
+
