2bc98f09d56f1d5fc82b8e4b3de5446275403a52
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-08-05 17:00:50 +0530

    Project Socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,32 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import { ConnectDB } from './lib/db.js';
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app=express();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials:true
-}))
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -34,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT,()=>{
-    console.log("Server is Running on Port:"+PORT);
-    ConnectDB();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
