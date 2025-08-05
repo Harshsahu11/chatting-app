3f08e0f3e6f777fd5ee517782c946aab612a2c12
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-08-05 11:48:50 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app=express();
 
 const PORT = process.env.PORT;
-const --dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
